d3ed51731e77e02a8e41475e63d94d2f1e80e358
ShenJimei <43808296+ShenJimei@users.noreply.github.com>
2019-01-15 12:38:32 +0800

    Add files via upload

---
+++ FirstDemo.py
@@ -0,0 +1,6 @@
+# @Author  : Eric_Lee！！
+# @Time    : 2018/11/9 上午11:25
+# @FileName: FirstDemo.py
+# @Software: PyCharm
+def predict(x):
+    print('听说班长今晚请吃饭')
---
+++ hello1.py
@@ -0,0 +1,7 @@
+# @Author  : Eric_Lee！！
+# @Time    : 2018/11/10 上午11:01
+# @FileName: hello1.py
+# @Software: PyCharm
+def hello(name):
+    print('Hello,', name)
+hello('Liyan')
